41850723d2e245f4c71b638a12fb82473f66b929
yashkhatriVIT <yashuk.1232@gmail.com>
2022-12-18 00:56:01 +0530

    added the serverside logic for mail

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+const port = process.env.PORT || 8093;
+app.listen(port, () => console.log("Server Running"));
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "yashuk.1232@gmail.com",
+    pass: "pnsxeevhgbbxlnxw"
+  },
+});
+
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
+}
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "yashuk.1232@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      return res.json(error);
+    } else {
+      return res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
